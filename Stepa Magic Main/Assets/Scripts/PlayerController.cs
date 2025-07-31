f7cfe55cf1904c217a05be2f5b4056e290a059ed
jonter <jonterrusian@gmail.com>
2025-07-31 16:48:52 +0300

    v 0.2 Setup Player Animations

--- Stepa Magic Main/Assets/Scripts/PlayerController.cs
+++ Stepa Magic Main/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
     [SerializeField] float maxSpeed = 8;
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         MovePlayer();
+        HandleAnim();
     }
 
     void MovePlayer()
@@ -25,6 +29,19 @@ public class PlayerController : MonoBehaviour
         Vector3 dir = new Vector3(inputX, 0, inputZ);
         if(dir.magnitude > 1) dir = dir.normalized;
         rb.velocity = dir * maxSpeed;
+    }
 
+    void HandleAnim()
+    {
+        float currentSpeed = rb.velocity.magnitude / maxSpeed;
+        anim.SetFloat("speed", currentSpeed);
+
+        Vector3 forward = transform.forward;
+        Vector3 vel = rb.velocity;
+        float angle = Vector3.Angle(forward, vel);
+        float dot = Vector3.Dot(vel, transform.right);
+        if (dot < 0) angle = -angle;
+        
+        anim.SetFloat("rotation", angle);
     }
 }
