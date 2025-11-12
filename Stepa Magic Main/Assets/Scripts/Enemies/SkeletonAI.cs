26359e9f9f7930d757609de03620195f42a050f0
jonter <jonterrusian@gmail.com>
2025-11-12 03:45:38 +0300

    v 1.4 Add Basic Ai for our enemy

--- Stepa Magic Main/Assets/Scripts/Enemies/SkeletonAI.cs
+++ Stepa Magic Main/Assets/Scripts/Enemies/SkeletonAI.cs
@@ -1,21 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
+using UnityEngine.Rendering;
+/// <summary>
+/// TODO 11.11.2025
+/// Разобраться с анимациями, чтобы остановка/разгон были более плавные
+/// Разобраться с атакой, сделать так, чтобы персонаж мог атаковать
+/// даже на скорости (при помощи Animator layer).
+/// У Степы немного колбасит врага, когда он рядом с игроком))
+/// </summary>
 public class SkeletonAI : EnemyAI
 {
-    // Start is called before the first frame update
-    IEnumerator Start()
+    [SerializeField] float rageRadius = 15;
+    [SerializeField] float damage = 20;
+    [SerializeField] float attackDistance = 1.5f;
+    
+
+    Animator anim;
+    NavMeshAgent agent;
+    bool isSeen = false;
+
+    PlayerHealth target;
+
+    void Start()
     {
-        Animator anim = GetComponent<Animator>();
-        anim.SetFloat("speed", 0);
-        yield return new WaitForSeconds(3);
-        anim.SetFloat("speed", 1);
-        yield return new WaitForSeconds(3);
-        anim.SetTrigger("attack");
-        yield return new WaitForSeconds(3);
-        StartCoroutine(Start());
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        target = FindAnyObjectByType<PlayerHealth>();
     }
 
-    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, rageRadius);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackDistance);
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+        if (distance < rageRadius) isSeen = true;
+
+        if (isSeen == true) ChaseTarget(distance);
+
+        SetAnimation(distance);
+    }
+
+    void SetAnimation(float distance)
+    {
+        float animSpeed = agent.desiredVelocity.magnitude / agent.speed;
+        anim.SetFloat("speed", animSpeed);
+    }
+
+    void ChaseTarget(float distance)
+    {
+        // если расстояние до игрока меньше дистанции атаки (0.9 от дистанции)
+        // то он будет останавливаться
+        if (distance < attackDistance * 0.9)
+            agent.destination = transform.position;
+        else 
+            agent.destination = target.transform.position;
+    }
+
+
+
 }
