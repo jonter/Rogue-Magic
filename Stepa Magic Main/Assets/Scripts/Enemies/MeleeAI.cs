5352981ccbbc705135ce2cb509f44c936c35661d
jonter <jonterrusian@gmail.com>
2025-12-02 18:30:23 +0300

    v 1.5 Add Melee AI for enemy

--- Stepa Magic Main/Assets/Scripts/Enemies/MeleeAI.cs
+++ Stepa Magic Main/Assets/Scripts/Enemies/MeleeAI.cs
@@ -10,24 +10,32 @@ using UnityEngine.Rendering;
 /// даже на скорости (при помощи Animator layer).
 /// У Степы немного колбасит врага, когда он рядом с игроком))
 /// </summary>
-public class SkeletonAI : EnemyAI
+public class MeleeAI : EnemyAI
 {
     [SerializeField] float rageRadius = 15;
-    [SerializeField] float damage = 20;
     [SerializeField] float attackDistance = 1.5f;
-    
 
+    [SerializeField] DamageType damageType = DamageType.Physical;
+    [SerializeField] float damage = 20;
+    
     Animator anim;
     NavMeshAgent agent;
     bool isSeen = false;
 
     PlayerHealth target;
 
+    float maxSpeed;
+    float currentSpeed;
+
+    bool isBusy = false;
+
     void Start()
     {
         anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         target = FindAnyObjectByType<PlayerHealth>();
+        currentSpeed = 0;
+        maxSpeed = agent.speed;
     }
 
     private void OnDrawGizmosSelected()
@@ -50,8 +58,10 @@ public class SkeletonAI : EnemyAI
 
     void SetAnimation(float distance)
     {
-        float animSpeed = agent.desiredVelocity.magnitude / agent.speed;
-        anim.SetFloat("speed", animSpeed);
+        float realSpeed = agent.desiredVelocity.magnitude / maxSpeed;
+        currentSpeed = Mathf.Lerp(currentSpeed, realSpeed, Time.deltaTime * 10);
+
+        anim.SetFloat("speed", currentSpeed);
     }
 
     void ChaseTarget(float distance)
@@ -59,11 +69,47 @@ public class SkeletonAI : EnemyAI
         // если расстояние до игрока меньше дистанции атаки (0.9 от дистанции)
         // то он будет останавливаться
         if (distance < attackDistance * 0.9)
+        {
             agent.destination = transform.position;
+            RotateToPlayer();
+        }
         else 
             agent.destination = target.transform.position;
+
+        if(distance < attackDistance * 1.5f)
+        {
+            if (isBusy == true) return;
+            StartCoroutine(AttackAnimCoroutine());
+        }
+    }
+
+    void RotateToPlayer()
+    {
+        Vector3 dir = target.transform.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation,
+            Time.deltaTime * 5);
     }
 
 
+    IEnumerator AttackAnimCoroutine()
+    {
+        anim.SetTrigger("attack");
+        isBusy = true;
+        yield return new WaitForSeconds(1.5f);
+        isBusy = false;
+    }
 
+    private void OnDisable()
+    {
+        agent.destination = transform.position;
+    }
+
+    public void Attack()
+    {
+        float distance = Vector3.Distance(transform.position, 
+            target.transform.position);
+        if (distance < attackDistance)
+            target.GetDamage(damage, damageType);
+    }
 }
