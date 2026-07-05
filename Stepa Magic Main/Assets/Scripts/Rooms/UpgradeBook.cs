dfe2a63a3335b469423782bafe9d4f93c7e2ec9a
Evgeniy Ternitskiy <jonterrusian@gmail.com>
2026-07-05 18:55:36 +0300

    v 3.0 Toggle light and global shadows. Add blob shadows and add pseudo shadows for level exit.

--- Stepa Magic Main/Assets/Scripts/Rooms/UpgradeBook.cs
+++ Stepa Magic Main/Assets/Scripts/Rooms/UpgradeBook.cs
@@ -10,11 +10,29 @@ public class UpgradeBook : MonoBehaviour
     [SerializeField] ParticleSystem circleVFX;
     bool isEnter = false;
     public event Action OnPickup;
+    Vector3 startRot;
+    float startY;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        startY = transform.localPosition.y;
+        startRot = transform.localRotation.eulerAngles;
+        transform.DOLocalMoveY(startY - 0.5f, 1).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+        StartCoroutine(BookFloatCoroutine());
+    }
+
+    IEnumerator BookFloatCoroutine()
+    {
+        Vector3 randRot = startRot + UnityEngine.Random.insideUnitSphere * 10;
+        transform.DOLocalRotate(randRot, 2).SetEase(Ease.InOutSine);
+        yield return new WaitForSeconds(2);
+        StartCoroutine(BookFloatCoroutine());
+    }
+
+    private void OnDisable()
+    {
+        transform.DOKill();
     }
 
     private void OnTriggerEnter(Collider other)
